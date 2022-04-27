06d5dfb6d7e6584dbf6afc39e50149a72a078095
DavidW2333 <david.wang023@gmail.com>
2022-04-27 12:31:49 +1200

    quiz presentation

--- src/domain/model.py
+++ src/domain/model.py
@@ -22,3 +22,41 @@ class User:
     @property
     def password(self) -> str:
         return self.__password
+
+
+class Questions:
+    # q_id = -1
+    # question = ""
+    # option1 = ""
+    # option2 = ""
+    # correctOption = -1
+
+    def __init__(self, q_id: int, question: str, option1: str, option2: str, correctoption: int):
+        self.__option1 = option1
+        self.__option2 = option2
+        self.__q_id = q_id
+        self.__question = question
+        self.__correctoption = correctoption
+
+    def getQ_id(self):
+        #print("helllllo")
+        return self.__q_id
+
+    def getQuestion(self):
+        #print("in queston")
+        return self.__question
+
+    def getOption1(self):
+        return self.__option1
+
+    def getOption2(self):
+        return self.__option2
+
+    def getCorrectionOption(self):
+        return self.__correctoption
+
+    def get_correct_option(self):
+        if self.__correctoption == 1:
+            return self.__option1
+        elif self.__correctoption == 2:
+            return self.__option2
--- src/quiz/quiz.py
+++ src/quiz/quiz.py
@@ -1,8 +1,35 @@
 from flask import Blueprint, render_template
 
+from src.domain.model import Questions
+
 quiz_blueprint = Blueprint('quiz_bp', __name__)
+
+
 @quiz_blueprint.route('/quiz', methods=['GET'])
 def quiz():
+    que = "It looks like your someone has attempted to access your bank account using your password. Please reset" + \
+          "your password following the link below:Reset password button links to  https://b775sgH5j4.co/hHjz6Gu"
+    print("1111")
+    q1 = Questions(1,
+                   "It looks like your someone has attempted to access your bank account using your password. Please reset" + \
+                   "your password following the link below:Reset password button links to  https://b775sgH5j4.co/hHjz6Gu",
+                   "yes",
+                   "No",
+                   1)
+    q2 = Questions(2,
+                   "$2,000 PAK'nSAVE reward! Congratulations, you are a winner!" + \
+                   "You qualify for a $2,000 PACK'nSAVE reward , this email is Our Official News Letter for your persual." + \
+                   "Remember, you are one of the only few selected. Registration is only possible for the next 24 " + \
+                   "hours!",
+                   "yes",
+                   "NO",
+                   1)
+
+    qlist = [q1, q2]
+
     return render_template(
         'quiz/module1.html',
-    ) 
+        questionlist=qlist,
+        example=que
+
+    )

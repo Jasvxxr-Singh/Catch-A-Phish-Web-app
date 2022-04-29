5d0702fbcf8661a6506f4c7ceb711742387328e5
DavidW2333 <david.wang023@gmail.com>
2022-04-29 23:25:57 +1200

    showing the wrong questions

--- src/quiz/quiz.py
+++ src/quiz/quiz.py
@@ -5,12 +5,14 @@ from flask import Blueprint, render_template
 from src.domain.model import Questions
 
 quiz_blueprint = Blueprint('quiz_bp', __name__)
-q1 = Questions(1,
+# we add our questions over here
+
+q1 = Questions(1,  # the id of the question
                "It looks like your someone has attempted to access your bank account using your password. Please reset" + \
                "your password following the link below:Reset password button links to  https://b775sgH5j4.co/hHjz6Gu",
-               "yes",
-               "No",
-               1)
+               "yes",  # option1
+               "No",  # option2
+               1)  # the correct answer
 q2 = Questions(2,
                "$2,000 PAK'nSAVE reward! Congratulations, you are a winner!" + \
                "You qualify for a $2,000 PACK'nSAVE reward , this email is Our Official News Letter for your persual." + \
@@ -19,8 +21,16 @@ q2 = Questions(2,
                "yes",
                "NO",
                1)
+q3 = Questions(3,
+               "Hi, Someone just used your password to sign into your Google Account." + \
+               "Information:[insert py dateTime.now][first_name][last_name]" + \
+               "Firefox browser Google stopped this sign-in attempt. You should change your password immediately." + \
+               "[Change password button links to https://support.google.com-scan78.co.ac/passswipe/245556gysuu7",
+               "yes",
+               "NO",
+               1)
 
-qlist = [q1, q2]
+qlist = [q1, q2, q3]
 
 
 @quiz_blueprint.route('/quiz', methods=['GET'])
@@ -41,17 +51,24 @@ submit_blueprint = Blueprint('submit_bp', __name__)
 def submit():
     print('here')
     correct_count = 0
+    wrong_questions = []
     for question in qlist:
         question_id = str(question.getQ_id())
         selected_option = request.values.get(question_id)
         correct_option = question.get_correct_option()
         if selected_option == correct_option:
             correct_count += 1
-            print("correct_count", correct_count)
+            # print("correct_count", correct_count)
+        else:
+            wrong_questions.append(question.getQ_id())
+
     correct_count = str(correct_count)
+    if len(wrong_questions) == 0:
+        wrong_questions = 0
     return render_template(
         'quiz/result.html',
-        quiz_result=correct_count
+        quiz_result=correct_count,
+        wrong_q=wrong_questions
     )
 
 
@@ -62,4 +79,5 @@ resolutions_blueprint = Blueprint('resolutions_bp', __name__)
 def results():
     return render_template(
         'quiz/resolutions.html'
+
     )

204cc9d903d70e997ccd11089a5fcd84216d20b9
Beeman26 <jsin2623@gmail.com>
2022-05-12 15:18:59 +1200

    added more to quiz2 bp

--- src/quiz/quiz2.py
+++ src/quiz/quiz2.py
@@ -17,10 +17,34 @@ def quiz2():
     q3 = (3, "")
 
     qlist = [q1, q2, q3]
+    total_number_of_questions = len(qlist)
+    question_chunks = utilities.get_chunks(qlist, 1)
+
+    page_number = request.args.get("page_number")
+
+    if page_number is None:
+        page_number = 0
+
+    page_number = int(page_number)
+    if page_number == 0:
+        previous_page = 0
+    else:
+        previous_page = page_number - 1
+
+    if page_number == len(question_chunks) - 1:
+        next_page = len(question_chunks) - 1
+    else:
+        next_page = page_number + 1
+
 
     return render_template(
-        'quiz2/module2.html'
+        'quiz2/module2.html',
         questionlist=qlist,
+        next_page=next_page,
+        prev_page=previous_page,
+        total_number_of_questions=total_number_of_questions,
+        current_page=page_number,
+
     )
 
 
@@ -28,5 +52,21 @@ submit_blueprint2 = Blueprint(submit_bp2, __name__)
 @submit_blueprint2.route('/submitquiz2', methods=['POST', 'GET'])
 def submit2():
     correct_count = 0
+
     for question in qlist:
-        question_id = str(question.
+        question_id = str(question.getQ_id())
+        print("questionid=", type(question_id))
+
+        selected_option = request.form.get(question_id)
+        print("selected_option=", selected_option)
+        correct_option = question.get_correct_option()
+        print("correct_option=", correct_option)
+
+        if selected_option == correct_option:
+            correct_count += 1
+        correct_count = str(correct_count)
+
+    return render_template(
+        'quiz/result.html',
+        quiz_result=correct_count
+    )
